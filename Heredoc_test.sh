955509cd069de8806a852451cc584adaabbe51b0
JOSEF JR ABABAT <ababatjosef@gmail.com>
2020-02-08 14:31:20 -0500

    Removed qoutes

--- Heredoc_test.sh
+++ Heredoc_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cat <<EOF
+cat<<-EOF
 =======================================================================================================
 What can this script do?:
-- Remove characters present in \"*.dat\" files such as: {, }, [, and ].
-- Relocate files hidden under \".ssh\" directory.
-- Update the timestamp of all the \"dat\" files it finds.
+- Remove characters present in "*.dat" files such as: {, }, [, and ].
+- Relocate files hidden under ".ssh" directory.
+- Update the timestamp of all the "dat" files it finds.
 - Shows you a list of files that were successfully uploaded(cleared) and files that needs manual check.
 - Sends email of the processing results.
 =======================================================================================================
